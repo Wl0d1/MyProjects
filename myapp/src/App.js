0b60806c98a6148f3681c70eca8b8be92a74b6a7
Tomasz <tomas_wlodarczyk@o2.pl>
2022-07-20 20:21:30 +0200

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,57 +1,66 @@
 import React, {useState} from 'react';
 import './App.css';
+// import styled from 'styled-components'
+
+// const Wrapper = styled.div`
+// background-color: #f7f8fa;
+// display: flex;
+// justify-content: center;
+// align-items: center;
+// width: 100%;
+// height: 100vh;
+// `;
 
 function App () {
+
+
 
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
 
-  function addItem() {
 
+  function addItem() {
     if (!newItem) {
-      alert("Wpisz coś gościu");
+      alert("wpisz cos gosciu")
       return;
     }
+
     const item = {
       id: Math.floor(Math.random() * 1000),
       value: newItem
     };
 
-    setItems(oldList => [...oldList,item]);
-    setNewItem("")
+    setItems(oldList => [...oldList, item]);
+    setNewItem("");
   }
 
-  function deleteItem (id) {
+    function deleteBtn (id) {
       const newArray = items.filter(item => item.id !== id);
-      setItems(newArray)
-  }
+      setItems(newArray);
+    }
 
-  return (  
-    <div className = "App"> 
-        <h1>To do List</h1>
+  return(
+    <div className="App">
+      <h1>To Do list </h1>
+      <input 
+      type="text"
+      placeholder="dodaj cos"
+      value= {newItem}
+      onChange={e => setNewItem(e.target.value)} 
+      />
 
-        <input 
-        type="text" 
-        placeholder="Title.." 
-        value={newItem}
-        onChange = {e => setNewItem(e.target.value)}>
-        
-        </input> 
-        <button onClick = {(e) => addItem()} >Add</button>
+      <button onClick={() => addItem()}> Add </button>
 
       <ul>
-            {items.map(item => {
-              return(
-                <li key={item.id}>{item.value} <button className="deleteBtn" onClick={() => deleteItem(item.id)}><i class="fa-solid fa-xmark"></i></button></li>
-              )
-            })}
-      </ul>
-
-    
+      {items.map(item => {
+        return (
+          <li key={item.id}>{item.value} <button onClick={() => deleteBtn (item.id)}><i class="fa-solid fa-xmark"></i></button>
+          </li>
+        )
+      })}  
+    </ul>
+  </div>
+  )
+};
   
-
-    </div>
-  );
-}
-
 export default App;
