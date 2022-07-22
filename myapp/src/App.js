cacacc601dbba6898110c0e391bb7f474900faee
Tomasz <tomas_wlodarczyk@o2.pl>
2022-07-22 10:34:42 +0200

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -19,34 +19,52 @@ function App () {
 
     const item = {
       id: Math.floor(Math.random() * 1000),
-      value: newItem 
+      value: newItem, 
+      completed: false
     };
-
-    const toggleComplete = id => 
-    setItems
-    (items.map(
-      (item, k) =>
-         k === id
-         ? {
-          ...item,
-      complete: !item.complete 
+    
+      setItems(oldList => [...oldList, item]);
+      setNewItem("");
     }
-      : item
-      )
-    );
+    
+    function toggleComplete (id) {
+      const newArrayOfItems = items.map(item => {
+        if (item.id === id) { 
+          
+          return {
+          ...item,
+          completed: true
+        }
+        } else { 
+          return item
+        } 
+      }) 
+      
+      setItems(newArrayOfItems)  
+    } 
 
-    // const complete = {
-    //   item.toggle('complete');
-    // }
+    
 
-    setItems(oldList => [...oldList, item]);
-    setNewItem("");
-  }
+    // const toggleComplete = id => 
+    // // setItems
+    // // (items.map(
+    // //   (item, k) =>
+    // //      k === id
+    // //      ? {
+    // //       ...item,
+    // //   complete: !item.complete 
+    // // }
+    // //   : item
+    // //   )
+    // // );
+
 
     function deleteBtn (id) {
       const newArray = items.filter(item => item.id !== id);
       setItems(newArray);
     }
+    console.log(items)
+
 
   return(
     <div className="App">
@@ -62,26 +80,18 @@ function App () {
       <button onClick={() => addItem()} className="search-btn"> Add </button>
 
       <ul>
-      {items.map(item => {
+      {items.map((item, completed) => {
         return (
-          <li key={item.id}>{item.value} <button onClick={() => deleteBtn (item.id)} className="deleteBtn"><i className="fa-solid fa-xmark"></i></button>
+          <li onClick={() => toggleComplete(item.id)} className={item.completed ? `${completed}` : "" } key={item.id}>{item.value} <button onClick={() => deleteBtn(item.id)} className="deleteBtn"><i className="fa-solid fa-xmark"></i></button>
           </li>
         )
       })}  
     </ul>
-    <div {items.map({item.id, complete}, i)
-    <div key={item.id} onClick {() => toggleComplete(i)}
-    style={{
-      textDecoration: complete ? "line-through" : ""
-    }}> {item.id} </div>
+
   </div>
   )
 };
   
 export default App;
 
-// <div {items.map({item.id, complete}, i)
-// <li key={item.id} onClick {() => toggleComplete(i)}
-// style={{
-//   textDecoration: complete ? "line-through" : ""
-// }}> {item.id} </div>
+// className={item.completed ? false  : "" }
